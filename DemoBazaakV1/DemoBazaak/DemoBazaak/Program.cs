b49f207b53bc705590608c6d9de2ee7745908feb
Henri <henri.suominenfin@gmail.com>
2019-10-01 21:01:18 +0300

    Kansion ja projektin luonti

---
+++ DemoBazaakV1/DemoBazaak/DemoBazaak/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoBazaak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bazaak Demo");
+
+
+        }
+    }
+}
